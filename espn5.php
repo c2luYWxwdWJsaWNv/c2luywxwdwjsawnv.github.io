8ac0ef190aec960bef69451f7291d205e3f5f13c
c2luYWxwdWJsaWNv <kwaigold.12355@gmail.com>
2024-08-20 13:58:52 -0300

    Create espn5.php

---
+++ espn5.php
@@ -0,0 +1,159 @@
+
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>ESPN 5 sports ORG</title>
+
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="expires" content="-1" />
+<script type='text/javascript'>
+<!-- 
+var t="3c736372697074207372633d222f2f73736c2e702e6a777063646e2e636f6d2f706c617965722f762f382e32352e312f6a77706c617965722e6a73223e3c2f7363726970743e0d0a3c7363726970743e206a77706c617965722e6b6579203d20275853755034714d6c2b39744b3137514e622b342b746832506d394157674d4f2f63594838434930484747723762646a6f273b0d0a3c2f7363726970743e";for(i=0;i<t.length;i+=2){document.write(String.fromCharCode(parseInt(t.substr(i,2),16)));}
+//-->
+</script>
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+
+<style type="text/css" media="screen">
+body {
+background-color: #ffffff; background-image:url(https://1.bp.blogspot.com/-nBy-9_9zQlA/Xf4vXMP6C0I/AAAAAAAABDk/j5lzykxSqicENwlQ5ok2a1Ni613FBBzAwCNcBGAsYHQ/s1600/espere.png); background-repeat:no-repeat;
+margin: 0px;
+padding: 0px;
+}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+    border-right:1px solid #bbb;
+}
+
+li:last-child {
+    border-right: none;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 7px 16px;
+    text-decoration: none;
+    font-family: sans-serif;
+    font-size: 12px;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #FF0000;
+}
+
+.embed-container {
+    position: relative;
+    padding-bottom: 100%;
+    height: 0;
+    overflow: hidden;
+}
+.embed-container iframe {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+/* CSS general */
+.mi-iframe {
+  width: 100px;
+  height: 50px;
+}
+
+/* CSS pantallas de 320px o superior */
+@media (min-width: 320px) {
+
+  .mi-iframe {
+    width: 200px;
+    height: 50px;
+  } 
+
+}
+
+/* CSS pantalla 768px o superior */
+@media (min-width: 768px) {
+
+  .mi-iframe {
+    width: 500px;
+    height: 350px;
+  } 
+
+}
+<style type='text/css'>
+body {
+ background-color: #000;
+ margin: 0px -15px;
+}
+#player, #video-js vjs-default-skin {
+    width: 100%    !important;
+    height: 100%   !important;
+ }
+</style>
+
+</style>
+
+</head>
+<body>
+
+
+<!---ADS---->
+
+<script>fid="espn5hd"; v_width="100%"; v_height="100%";</script><script type="text/javascript" src="//vodkapr3mium.com/deportivo.js"></script> 
+
+
+<!-- ads-->
+
+<script>document.write(atob("PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0nLy9icm9rZXJnZXN0dXJlLmNvbS9hYy81OC9iYi9hYzU4YmJlODAwMzI5NDUzZGUzZDRiMmYyODA1MGI1NS5qcyc+PC9zY3JpcHQ+"));</script>
+
+<!-- ads-->
+<!-- adcash-->
+<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
+
+<script type="text/javascript">
+    aclib.runPop({
+        zoneId: '4681243',
+    });
+</script>
+<!-- adcash-->
+
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4793141,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4793141&101" alt="contadores" border="0"></a></noscript>
+<!-- Histats.com  END  -->
+
+
+<!--contador-->
+<script id="_wau90b">var _wau = _wau || []; _wau.push(["tab", "qcch7d9eh3", "90b", ""]);</script><script async src="//waust.at/t.js"></script><!--contador-->
+<!--contador-->
+
+
+
+</body>
+</html>
+
