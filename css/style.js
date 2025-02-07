c0e50813962c9fb18a0121053c86c03cc56a9e8f
c2luYWxwdWJsaWNv <kwaigold.12355@gmail.com>
2025-02-07 16:07:03 -0300

    Update style.js

--- css/style.js
+++ css/style.js
@@ -1,38 +1,38 @@
-// 
 function getParametroUrl(paramName) {
   const urlParams = new URLSearchParams(window.location.search);
   return urlParams.get(paramName);
 }
 
-// 
 async function configurarPlayer(encodedUrl) {
   try {
-    // 
+    // Decodificando a URL recebida
     const decodedUrl = atob(encodedUrl);
 
-    // 
+    // Acessando a página do vídeo
     const fullUrl = "https://consoledplay.vercel.app/" + encodeURIComponent(decodedUrl);
 
-    // 
+    // Fazendo a requisição para a URL do vídeo
     const response = await fetch(fullUrl);
     const htmlContent = await response.text();
 
-    // 
-    const videoSrcPattern = /src\s*:\s*"([^"]+)"/i;
-    const videoSrcMatch = htmlContent.match(videoSrcPattern);
+    // Capturando o token
+    const tokenPattern = /token:\s*"([^"]+)"/i;
+    const tokenMatch = htmlContent.match(tokenPattern);
 
-    if (videoSrcMatch) {
-      
-      const videoUrl = videoSrcMatch[1];
+    if (tokenMatch) {
+      const token = tokenMatch[1]; // Captura o valor do token
 
-      // 
+      // Construindo a URL final do vídeo
+      const videoUrl = `https://fonteruim.icu/token/${token}/embedtv.m3u8`;
+
+      // Configurando o player com a URL gerada
       const player = jwplayer("player");
       player.setup({
         'playlist': [{
           'sources': [{
             'default': false,
             'type': "hls",
-            'file': videoUrl,  
+            'file': videoUrl,
             'label': '0'
           }]
         }],
@@ -43,17 +43,19 @@ async function configurarPlayer(encodedUrl) {
         'cast': true,
         'sharing': false
       });
+    } else {
+      console.error("Token não encontrado na página.");
     }
   } catch (error) {
     console.error("Erro ao configurar o player:", error);
   }
 }
 
-// 
+// Espera a página carregar
 document.addEventListener("DOMContentLoaded", function () {
   const paramId = getParametroUrl('id');
   if (paramId) {
-    // 
+    // Configura o player com o parâmetro 'id'
     configurarPlayer(paramId);
   }
 });
